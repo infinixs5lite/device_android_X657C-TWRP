037956e460e90c631630c48b60a5658a9f49b9e2
Miracle <127039117+Miraclev1@users.noreply.github.com>
2023-03-29 00:22:10 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/Infinix-X657C
+DEVICE_PATH := device/infinix/Infinix-h6117
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
@@ -24,7 +24,7 @@ TARGET_USES_64_BIT_BINDER := true
 OVERRIDE_TARGET_FLATTEN_APEX := true
 
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := Infinix-X657C
+TARGET_BOOTLOADER_BOARD_NAME := Infinix-h6117
 TARGET_NO_BOOTLOADER := true
 
 # Display
@@ -43,8 +43,8 @@ BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DTBO := true
-TARGET_KERNEL_CONFIG := Infinix-X657C_defconfig
-TARGET_KERNEL_SOURCE := kernel/infinix/Infinix-X657C
+TARGET_KERNEL_CONFIG := Infinix-h6117_defconfig
+TARGET_KERNEL_SOURCE := kernel/infinix/Infinix-h6117
 
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
