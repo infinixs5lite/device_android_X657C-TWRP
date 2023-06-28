50d28b403c9da1ee1a4fec2e8d1126e7799463d4
infinixs5lite <kelvinchinedu97@gmail.com>
2023-06-28 18:25:17 +0100

    Update omni_X657C.mk

--- omni_X657C.mk
+++ omni_X657C.mk
@@ -23,10 +23,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-
-# Inherit from X657C device
-$(call inherit-product, device/infinix/X657C/device.mk)
-
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
     libpuresoftkeymasterdevice
