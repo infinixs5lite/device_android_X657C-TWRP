877b1fec9e944743065861df10e426fe68ed6020
infinixs5lite <kelvinchinedu97@gmail.com>
2023-06-30 21:29:29 +0100

    Update omni_X657C.mk

--- omni_X657C.mk
+++ omni_X657C.mk
@@ -6,12 +6,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Define Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
- Fastbootd
+# Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
