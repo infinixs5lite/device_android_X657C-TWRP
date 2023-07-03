d15246fab9e1a0ae17aa796754ce4e898c659718
infinixs5lite <kelvinchinedu97@gmail.com>
2023-07-03 21:43:22 +0100

    Update omni_X657C.mk

--- omni_X657C.mk
+++ omni_X657C.mk
@@ -25,12 +25,6 @@ PRODUCT_MANUFACTURER := infinix mobility
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
 
-# enable stock zip packages flash
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.treble.enabled=true \
-    persist.sys.usb.config=mtp \
-    persist.service.adb.enable=1 \
-    persist.service.debuggable=1 \
-    ro.secure=1 \
-    ro.adb.secure=0 \
-    ro.allow.mock.location=0
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
